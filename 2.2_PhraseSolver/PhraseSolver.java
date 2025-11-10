d46c5dae0239b20d523068bb6e3d09eb56892632
Myo Thura Ko <myothura.ko@crpusd.org>
2025-11-10 09:18:53 -0800

    updated

--- 2.2_PhraseSolver/PhraseSolver.java
+++ 2.2_PhraseSolver/PhraseSolver.java
@@ -1,19 +1,20 @@
+
 /*
  * Activity 2.2.2
  *
  *  The PhraseSolver class the PhraseSolverGame
  */
 import java.util.Scanner;
-  
-public class PhraseSolver
-{
+
+public class PhraseSolver {
   /* your code here - attributes */
   private Player player1;
   private Player player2;
   private Board game;
   private boolean solved;
-  /* your code here - constructor(s) */ 
-  public PhraseSolver(){
+
+  /* your code here - constructor(s) */
+  public PhraseSolver() {
     player1 = new Player();
     player2 = new Player();
     game = new Board();
@@ -23,24 +24,22 @@ public class PhraseSolver
     game = null;
     solved = false;
   }
-  public void play()
-  {
+
+  public void play() {
     boolean solved = false;
     int currentPlayer = 1;
 
     Scanner input = new Scanner(System.in);
-    
+
     boolean correct = true;
-    while (!solved) 
-    {
-      
+    while (!solved) {
+
       /* your code here - game logic */
-      
-      
+
       /* your code here - determine how game ends */
-      solved = true; 
-    } 
-   
+      solved = true;
+    }
+
   }
-  
+
 }
--- 2.2_PhraseSolver/Player.java
+++ 2.2_PhraseSolver/Player.java
@@ -1,3 +1,4 @@
+
 /*
  * Activity 2.2.2
  * 
@@ -5,26 +6,27 @@
  */
 import java.util.Scanner;
 
-public class Player
-{
+public class Player {
   /* your code here - attributes */
   private String name;
   private int points;
-  /* your code here - constructor(s) */ 
-  public Player(){ /// constructor, no input at all
+
+  /* your code here - constructor(s) */
+  public Player() { /// constructor, no input at all
     Scanner sc = new Scanner(System.in);
     System.out.println("what is your name?");
     name = sc.nextLine();
     points = 0;
     System.out.println("welcome" + name + ", to the game");
   }
+
   public Player(String inputName) {// contructor, uses parameter
-        name = inputName;
-        points = 0;
-        System.out.println("welcome " + name + ", to the game");
-    }
+    name = inputName;
+    points = 0;
+    System.out.println("welcome " + name + ", to the game");
+  }
 
-  /* your code here - accessor(s) */ 
+  /* your code here - accessor(s) */
 
-  /* your code here - mutator(s) */ 
+  /* your code here - mutator(s) */
 }
